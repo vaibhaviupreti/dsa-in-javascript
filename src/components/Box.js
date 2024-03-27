a1444a05ee7c726ee6f2295d7de596ace43f2ef9
Vaibhavi Upreti <vaibhaviu.ind@gmail.com>
2024-03-27 22:21:03 +0530

    1st commit - dsa - to push structure into git repo

---
+++ src/components/Box.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Box = () => {
+  return (
+    <div style={{display:'flex',justifyContent:'center',alignItems:'center',width:'100vw',height:'100vh'}}>
+        <div style={{display:'flex',justifyContent:'center',alignItems:'center',width:'50%',height:'auto',border:'1px solid #000',padding:'20px',borderRadius:'7px'}}>
+            hello
+        </div>
+    </div>
+  )
+}
